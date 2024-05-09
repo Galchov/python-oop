3210afe8eef523628791de13361853af4edbed4b
Zdravko Galchov <zdravko.galchov@gmail.com>
2024-05-09 18:58:56 +0100

    Added lesson - Inheritance, with examples

---
+++ 03_inheritance/lab/05_hierarchical_inheritance/project/test.py
@@ -0,0 +1,16 @@
+from project.cat import Cat
+from project.dog import Dog
+
+
+# Test code:
+cat = Cat()
+print(cat.meow())
+print(cat.eat())
+dog = Dog()
+print(dog.bark())
+print(dog.eat())
+
+"""
+This is a simple example of Hierarchical Inheritance - When multiple child classes 
+inherit from a single parent class.
+"""
